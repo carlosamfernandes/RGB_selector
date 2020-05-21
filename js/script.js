961b342493da1a34e98ba2e34f5382a00bd90fce
Carlos Fernandes <carlosamfernandes@gmail.com>
2020-05-21 16:26:35 -0300

    Changed the word colour (british) to color (american) to padronize the code

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ let redSelector = document.querySelector('#red');
 let greenSelector = document.querySelector('#green');
 let blueSelector = document.querySelector('#blue');
 
-let circleDiv = document.querySelector('#colour-circle');
+let circleDiv = document.querySelector('#color-circle');
 
 let span = document.querySelector('#rgb-text');
 
-function setColour() {
+function setColor() {
   document.querySelector('#numbers-red').value = redSelector.value;
   document.querySelector('#numbers-green').value = greenSelector.value;
   document.querySelector('#numbers-blue').value = blueSelector.value;
@@ -24,47 +24,6 @@ function setColour() {
   //console.log(span.textContent);
 }
 
-redSelector.addEventListener('input', setColour);
-greenSelector.addEventListener('input', setColour);
-blueSelector.addEventListener('input', setColour);
-
-/////////////////.
-/*
-
-function teste() {
-  redSelector.addEventListener('input', setColour, console.log(red.value)),
-    greenSelector.addEventListener('input', setColour,console.log(green.value)),
-    blueSelector.addEventListener('input', setColour, console.log(blue.value));
-}
-*/
-/////////////////////////
-/*
-redSelector.addEventListener('input', function () {
-  red = this.value;
-  document.querySelector('#numbers-red').value = red;
-});
-
-greenSelector.addEventListener('input', function () {
-  var green = this.value;
-  document.querySelector('#numbers-green').value = green;
-});
-
-blueSelector.addEventListener('input', function () {
-  var blue = this.value;
-  document.querySelector('#numbers-blue').value = blue;
-});
-*/
-//////////////////////////////////
-
-//console.log(red, green, blue);
-
-/*
-function setColour(event) {
-    var colour = this.value;
-    //var colour = event.target.value;   // tambem funciona
-    console.log(colour);
-  }
-  redSelector.addEventListener('input', setColour);
-  greenSelector.addEventListener('input', setColour);
-  blueSelector.addEventListener('input', setColour);
-  */
+redSelector.addEventListener('input', setColor);
+greenSelector.addEventListener('input', setColor);
+blueSelector.addEventListener('input', setColor);
